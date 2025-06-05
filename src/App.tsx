1d807d694d6f95a345012737962010a570196b0c
Cephaz6 <cephasblog@gmail.com>
2025-06-05 17:31:03 +0100

    DevTask Tracker Client Scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black">
+      <h1 className="font-base text text-3xl text-white">
+        Welcome to DevTask Tracker 🎊
+      </h1>
+    </div>
+  );
+};
+
+export default App;
